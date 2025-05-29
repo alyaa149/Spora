7f2e304d3879def4cddd333974acc73f10b4016b
alyaa149 <lohussien2@gmail.com>
2025-05-30 02:18:29 +0300

    home and splash

---
+++ Pods/Target Support Files/Pods-SporaApp-SporaAppUITests/Pods-SporaApp-SporaAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SporaApp_SporaAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SporaApp_SporaAppUITests
+@end
---
+++ SporaApp/View/controller/category/CategoryCollectionViewController.swift
@@ -0,0 +1,75 @@
+//
+//  CategoryCollectionViewController.swift
+//  SporaApp
+//
+//  Created by macOS on 29/05/2025.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CategoryCollectionViewController: UICollectionViewController,UICollectionViewDelegateFlowLayout {
+    let sports = [
+           ("Football", UIImage(named: "football")),
+           ("Basketball", UIImage(named: "basketball")),
+           ("Tennis", UIImage(named: "tennis")),
+           ("Cricket", UIImage(named: "cricket"))
+       ]
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         self.title = "Sports Categories"
+         collectionView.backgroundColor = .white
+         let nib = UINib(nibName: "CategoryCollectionViewCell", bundle: nil)
+         collectionView.register(nib, forCellWithReuseIdentifier: "CategoryCollectionViewCell")
+       
+
+     }
+
+
+
+     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+         return sports.count
+     }
+
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let spacing: CGFloat = 10
+        let sideInset: CGFloat = 16
+        let totalSpacing = (2 * sideInset) + spacing
+        let availableWidth = collectionView.bounds.width - totalSpacing
+        let cellWidth = floor(availableWidth / 2)
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 130, left: 16, bottom: 0, right: 16)
+    }
+
+
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCollectionViewCell", for: indexPath) as? CategoryCollectionViewCell else {
+              return UICollectionViewCell()
+          }
+        let (title, image) = sports[indexPath.item]
+
+        cell.configure(with: title, image: image)
+
+        return cell
+    }
+
+    
+     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                         minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+         return 10
+     }
+
+     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                         minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+         return 10
+     }
+ 
+}
